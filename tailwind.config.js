90944570fc52a5329e84fd06a774524b6ca37043
strombraaten <ssb@variant.no>
2024-09-29 15:23:01 +0200

    Did way too many changes for one commit, but here we are

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,20 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'primary': '#0066cc',
+        lochmara: {
+          50: '#F3FAFF',
+          100: '#D2EDFF',
+          200: '#90D1FF',
+          300: '#4EB6FF',
+          400: '#0B9BFF',
+          500: '#0076C8',
+          600: '#005E9F',
+          700: '#004676',
+          800: '#002E4E',
+          900: '#001625',
+          950: '#000A10',
+        },
+        primary: '#0066cc',
         'primary-dark': '#66b3ff',
         'background-dark': '#1a1a1a',
         'text-dark': '#f0f0f0',
@@ -16,5 +29,5 @@ module.exports = {
     },
   },
   plugins: [],
-  darkMode: 'class', // Change this line from 'media' to 'class'
+  darkMode: 'class',
 }
