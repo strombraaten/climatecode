0be7ecde294ca23019e8b95f8b3e50854d22624c
strombraaten <ssb@variant.no>
2024-10-14 14:46:17 +0200

    added i18n
    As part of the internationalization

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,4 +3,5 @@ import { docsSchema } from '@astrojs/starlight/schema';
 
 export const collections = {
   docs: defineCollection({ schema: docsSchema() }),
+  i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
 };
